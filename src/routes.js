2ad5824c97c74d21b9831a0de83f11a86c909aee
colivie000 <casterlyo@gmail.com>
2023-04-28 13:30:28 -0400

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+// const addModels = require('./middleware/add-models');
+// Import Controllers
+
+const router = express.Router();
+
+// Add Models to the Router
+// router.use(addModels);
+
+// Define Routes
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const logRoutes = require('./middleware/log-routes');
+const routes = require('./routes');
+
+const app = express();
+console.log(__dirname);
+const publicDir = path.join(__dirname, '..', 'public');
+
+// Remember, order matters when registering middleware and routes!
+app.use(logRoutes);
+app.use(express.json());
+app.use(express.static(publicDir));
+
+app.use(routes);
+
+module.exports = app;
