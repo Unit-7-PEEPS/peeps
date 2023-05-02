74051ad59cd1617b411f2ce2a7e6bb79fa9f2cea
colivie000 <casterlyo@gmail.com>
2023-05-02 09:58:39 -0400

    migrations

---
+++ src/db/migrations/20230501075954_community.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('community', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.integer('creator_id').notNullable();
+        table.foreign('creator_id').references('id').inTable('users');
+        table.timestamp('created_at', { useTz: true }).notNullable();
+        table.string('summary');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('community');
+};
